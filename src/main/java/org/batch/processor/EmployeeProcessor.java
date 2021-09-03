accfd3b9a657256390a3cdbf6230ac0778be23a6
joubamatrixx <habbachimahjoub3@gmail.com>
2021-09-03 16:19:37 +0200

    methode qui filtre les employee par age

--- src/main/java/org/batch/processor/EmployeeProcessor.java
+++ src/main/java/org/batch/processor/EmployeeProcessor.java
@@ -11,11 +11,24 @@ public class EmployeeProcessor implements ItemProcessor<EmployeeDto, Employee> {
 	@Override
 	public Employee process(EmployeeDto item) throws Exception {
 		Employee emp = new Employee();
+		if(filterEmployeeByAge(25, item))
+			return emp = null;
+		
 		emp.setFirstname(item.getFirstname());
 		emp.setLastname(item.getLastname());
 		emp.setEmail(item.getEmail());
 		emp.setAge(item.getAge());
 		return emp;
+	}
+	
+	
+	public boolean filterEmployeeByAge( int ageLimit , EmployeeDto emp) {
+		if(emp.getAge() < ageLimit)
+			return true;
+		else {
+			return false;	
+		}
+		
 	}
 
 }
